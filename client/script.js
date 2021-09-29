5a724d7272a2244b9a2b458dcf28cc8da83f36ab
anasokasha-chambaam <anasokashachama@gmail.com>
2021-09-30 00:45:23 +0300

    Lorem

--- client/script.js
+++ client/script.js
@@ -1,27 +1,74 @@
-// import {io} from 'socket.io-client'; 
+// import {io} from 'socket.io-client';
+import io from "socket.io-client/dist/socket.io.js";
 
+// Global varaibales
 const joinRoomButton = document.getElementById("room-button");
 const messageInput = document.getElementById("message-input");
 const roomInput = document.getElementById("room-input");
 const form = document.getElementById("form");
 
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    const message = messageInput.value;
-    const room = roomInput.value;
-    console.log(messageInput);
-    if (message === "") return;
- 
-    displayMessage(message);
-    messageInput.value = "";
-});
+const socket = io("http://localhost:3080"); // 'admin' namespace
+const userSocket = io("http://localhost:3080/user"); // 'user' namespace
+// Global variables end
 
-joinRoomButton.addEventListener("click", () => {
-    const room = roomInput.value;
-})
+// Event listeners functions
+/**
+ * @description - 'rescieve-message' event listener: it displays rescieved message.
+ * @param message - The rescieved message.
+ */
+let socketRescieveFunc = (message) => {
+  displayMessage(message);
+};
+
+/**
+ * @description - 'connect' event listener: it diplays a message that user is connected.
+ */
+let connectCB = () => {
+  displayMessage("You'r connected with the id: " + socket.id);
+  socket.on("rescieve-message", socketRescieveFunc);
+}
+
+/**
+ * @description - user socket 'connect' event listener: it diplays a message that user is connected to 'user' namespace.
+ */
+let userConnectCB = () => {
+  alert('connected');
+}
+
+/**
+ * @description - If there is a message, it displays it, send it to all other users and resets the input value to nothing.
+ * @param {Object} e - 'submit' event object.
+ */
+let formSubmitCB = (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  const room = roomInput.value;
+  if (message === "") return;
+
+  displayMessage(message);
+  socket.emit("send_message", message, room);
+  messageInput.value = "";
+};
+
+/**
+ * @description - Join room with specific id.
+ */
+let joinRoomButtonCB = () => {
+  const room = roomInput.value;
+  socket.emit("join-room", room.trim(), displayMessage);
+};
+// Event listeners functions end
+// Event listenerd
+socket.on("connect", connectCB);
+
+userSocket.on('connect', userConnectCB);
+
+form.addEventListener("submit", formSubmitCB);
+
+joinRoomButton.addEventListener("click", joinRoomButtonCB);
 
 function displayMessage(message) {
-    const div = document.createElement("div");
-    div.textContent = message;
-    document.getElementById("message-container").append(div);
+  const div = document.createElement("div");
+  div.textContent = message;
+  document.getElementById("message-container").append(div);
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,77 @@
-const webSocketIo = require('socket.io')(3080);
+// Global variables
+const { instrument } = require("@socket.io/admin-ui");
+const webSocketIo = require("socket.io")(3080, {
+  cors: {
+    origin: ["http://localhost:8080", "https://admin.socket.io"],
+  },
+});
+const userIO = webSocketIo.of('/user');
+// Global variables end
 
-webSocketIo.on('connection',socket => {
-    console.log(socket.id);
+// callbacks start
+
+/**
+ * @description - 'send-message' event call back: it takes message from the sender and send it to all other users, specific user, or users of a specific room.
+ * @param message - The message sended by user.
+ * @param room - The specific user or room id to send message to .
+ */
+const sendMessageCB = (message, room) => {
+  if (room.trim().length > 0) {
+    console.log(room);
+    socket.broadcast.to(room.trim()).emit("rescieve-message", message);
+    return;
+  }
+  socket.broadcast.emit("rescieve-message", message);
+};
+
+/**
+ * @description - 'join-room' event call back: it makes user join the specified room and runs a callback.
+ * @param room - The room name to join to.
+ * @param cb - The callback function to run after joining (It's usually a notification or a message to tell user that he joined succefully).
+ */
+const joinRoomCB = (room, cb) => {
+  socket.join(room);
+  cb("You are connected to (( " + room + " ))");
+};
+
+/**
+ * @description - 'connection' event call back: It prints the id of the connected user along with running event listeners.
+ * @param socket - The connected socket info.
+ */
+const connectionCB = (socket) => {
+  console.log(socket.id);
+
+  /**
+   * @description - Socket 'send-message' listener.
+   * @param event - The event we want listen to.
+   * @param Listener - The call back to run on the event.
+   */
+  socket.on("send_message", sendMessageCB);
+
+  /**
+   * @description - Socket 'join-room' listener.
+   * @param event - The event we want listen to.
+   * @param Listener - The call back to run on the event.
+   */
+  socket.on("join-room", joinRoomCB);
+};
+
+// callbacks end
+
+
+userIO.on('connection', userSocket => {
+    console.log('You are connected to user namespace "'+userSocket.id+'".')
 })
 
-console.log('Hi');
+/**
+ * @description - Socket 'connection' listener.
+ * @param event - The event we want listen to.
+ * @param Listener - The call back to run on the event.
+ */
+webSocketIo.on("connection", connectionCB);
+
+console.log("Hi");
+
+instrument(webSocketIo, {
+  auth: false,
+});
